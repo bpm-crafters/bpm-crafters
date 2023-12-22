0c4327c3040e3e9246e3cf130c4e9048bb31aca0
Hafflgav <52525.thomas@gmail.com>
2023-12-22 13:06:51 +0100

    fix: disable color mode switch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -99,7 +99,9 @@ const config: Config = {
     },
     prism: {
       theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
+    },
+    colorMode: {
+      disableSwitch: true,
     },
   } satisfies Preset.ThemeConfig,
 };
