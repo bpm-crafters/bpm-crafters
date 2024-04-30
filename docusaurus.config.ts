e4917d4177ebf6b75d739c1a12957761e0045a84
Simon Zambrovski <simon.zambrovski@holisticon.de>
2024-04-30 14:09:55 +0200

    trying to fix CNAME

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,14 +38,14 @@ const config: Config = {
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                        'https://github.com/bpm-crafters/bpm-crafters/edit/main/docs/'
                 },
                 blog: {
                     showReadingTime: true,
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                        'https://github.com/bpm-crafters/bpm-crafters/edit/main/docs/',
                 },
                 theme: {
                     customCss: './src/css/custom.css',
